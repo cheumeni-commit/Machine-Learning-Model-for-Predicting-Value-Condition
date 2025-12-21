341b385f883fc56e87a3540d49b7a84a3239cb2f
Jean-Michel <manssah.regulus.ai@gmail.com>
2025-12-21 06:02:23 +0100

    New training

--- src/training/models.py
+++ src/training/models.py
@@ -15,6 +15,7 @@ logger.setLevel(logging.INFO)
 
 # Models registry
 _MODELS_REGISTRY_ = {'RandomForestClassifier': RandomForestClassifier,
+                     'xgb_Classifier': xgb.XGBClassifier,
                      'SGDClassifier': SGDClassifier
                      }
 
